38ec2d8a6d221c6ce3485a04f7a6f44a327cf4db
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-10-06 19:13:51 +0600

    shopping cart included

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Product.css';
-const Product = ({ product }) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+const Product = ({ product, handleAddToCart }) => {
     const { name, img, price, seller, ratings } = product;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,8 +14,10 @@ const Product = ({ product }) => {
                 <p><small>Seller : {seller}</small></p>
                 <p><small>Ratings :   {ratings} stars</small></p>
             </div>
-            <button className='btn-cart'>
-                <p>Add to Cart</p>
+            <button onClick={() => handleAddToCart(product)}
+                className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
